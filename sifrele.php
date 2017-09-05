b36c5afa0b6f68b4d2e22e0ae0cfdd75202d71ce
esckralpcd <31617512+esckralpcd@users.noreply.github.com>
2017-09-06 01:00:46 +0300

    Update sifrele.php

--- sifrele.php
+++ sifrele.php
@@ -4,11 +4,15 @@
 
 echo "By eSckRal\n";
 echo "Kelimeyi giriniz:";
+if($plaintext == help){
+echo "Kelimeyi giriniz yazan yere crypt etmek istediğiniz kelimeyi girip enterleyiniz.";
+
+}
 $plaintext = trim(fgets(STDIN, 1024));
 $ciphertext = md5($plaintext);
 $result = md5($ciphertext);
 
-
+echo "Sonuc:";
 echo "$result\n";
 
 
