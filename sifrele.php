37f562fa7867edb451815287b32caf026523a5c3
esckralpcd <31617512+esckralpcd@users.noreply.github.com>
2017-09-06 01:33:17 +0300

    Update sifrele.php

--- sifrele.php
+++ sifrele.php
@@ -1,22 +1,21 @@
 <?php
+
+
+
 echo "By eSckRal\n";
-echo "Modulununu Seciniz:";
-
-
-switch ($sec = trim(fgets(STDIN, 1024)) {
-
-
-
-case 1:
-echo "MD5 Sifreler."
+echo "Kelimeyi giriniz:";
 $plaintext = trim(fgets(STDIN, 1024));
 $ciphertext = md5($plaintext);
 $result = md5($ciphertext);
+
 echo "Sonuc:";
 echo "$result\n";
-break;
 
-}
+
+
+
+
+
 
 
 
