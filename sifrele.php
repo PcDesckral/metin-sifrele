0990be704ca2d6117c7ad60f57a36523e1b16394
esckralpcd <31617512+esckralpcd@users.noreply.github.com>
2017-09-06 01:03:56 +0300

    Update sifrele.php

--- sifrele.php
+++ sifrele.php
@@ -2,14 +2,8 @@
 
 
 
-echo "By Ahmet Burak KOSE\n";
+echo "By eSckRal\n";
 echo "Kelimeyi giriniz:";
-if($plaintext == help){
-echo "Kelimeyi giriniz yazan yere crypt etmek istediğiniz kelimeyi girip enterleyiniz.";
-}
-else {
-echo "Yanlis Komut Giridinz.";
-}
 $plaintext = trim(fgets(STDIN, 1024));
 $ciphertext = md5($plaintext);
 $result = md5($ciphertext);
