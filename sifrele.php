88c68e432f928fcd3174f0069f95fa0c8d70212c
esckralpcd <31617512+esckralpcd@users.noreply.github.com>
2017-09-06 00:49:51 +0300

    Update sifrele.php

--- sifrele.php
+++ sifrele.php
@@ -3,7 +3,7 @@
 
 
 echo "By eSckRal" . '<br>';
-echo "Kelimeyi giriniz:"
+echo "Kelimeyi giriniz:";
 $plaintext = trim(fgets(STDIN, 1024));
 $ciphertext = md5($plaintext);
 $result = md5($ciphertext);
