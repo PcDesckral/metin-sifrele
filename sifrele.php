7d1194f8f2927b9b2b9b90d1a0aa0dfbe53e6c43
esckralpcd <31617512+esckralpcd@users.noreply.github.com>
2017-09-06 01:17:37 +0300

    Update sifrele.php

--- sifrele.php
+++ sifrele.php
@@ -8,12 +8,14 @@ switch (1) {
 
 
 case 1:
+echo "MD5 Sifreler.";
+echo "Kelimeyi giriniz";
 $plaintext = trim(fgets(STDIN, 1024));
 $ciphertext = md5($plaintext);
 $result = md5($ciphertext);
 echo "Sonuc:";
 echo "$result\n";
-
+break;
 
 }
 
